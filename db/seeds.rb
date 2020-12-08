68b56e97ae56ee09215d5b3bb9cfa7ba0ae2cedb
laurencun <lauren.busbee@gmail.com>
2020-12-08 13:15:43 -0600

    fixed create todo bug

--- db/seeds.rb
+++ db/seeds.rb
@@ -3,7 +3,7 @@ require 'faker'
 User.delete_all
 Todo.delete_all
 
-15.times do 
+5.times do 
     User.create(
         username: Faker::Name.name
     )
@@ -11,11 +11,12 @@ Todo.delete_all
 end
 
 User.all.each do |user| 
-    10.times do 
+    5.times do 
         Todo.create(
             title: Faker::Lorem.sentence(word_count: 3),
             content: Faker::Lorem.sentence(word_count: 12),
             completed: false,
+            starred: false,
             user_id: user.id
         )
     end
