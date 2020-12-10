713fc547e99f801826c92e0dc4d69a1b686b2fe9
laurencun <lauren.busbee@gmail.com>
2020-12-10 12:52:05 -0600

    JWT auth completed

--- app/controllers/auth_controller.rb
+++ app/controllers/auth_controller.rb
@@ -5,10 +5,25 @@ class AuthController < ApplicationController
         if user && user.authenticate(params[:password])
             #checking password matches
             #returns false or the user object
-            render json: {id: user.id, username: user.username}   
+            #create jwt token
+            payload = {user_id: user.id}
+            token = JWT.encode(payload, 'my_secret', 'HS256')
+            
+            render json: {user: {id: user.id, username: user.username}, token: token}   
         else 
             render json: {error: 'Invalid username/password'}, status: 401
          end
+    end
+
+    def show
+        #grab token from headers and decode
+        #find validataed user and send user obj back in json
+        token = request.headers[:Authorization].split(' ')[1]
+        decoded_token = JWT.decode(token, 'my_secret', true, {algorithim: 'HS256'})
+        user_id = decoded_token[0]['user_id']
+        user = User.find(user_id)
+
+        render json: {user: {id: user.id, username: user.username}}   
     end
 
 end
--- config/routes.rb
+++ config/routes.rb
@@ -1,4 +1,5 @@
 Rails.application.routes.draw do
   resources :todos
   post '/auth', to: 'auth#create'
+  get '/current_user', to: 'auth#show'
 end
