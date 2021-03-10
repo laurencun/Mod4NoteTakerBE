1ec71cb54c183c7919e31b3e78edb86bdf6d6ff0
laurencun <lauren.busbee@gmail.com>
2021-03-10 15:40:51 -0600

    added jwt auth to create user method

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -8,6 +8,17 @@ class UsersController < ApplicationController
 
     def create 
         user = User.create(username: params[:username], password: params[:password])
+        if user.save
+            #checking password matches
+            #returns false or the user object
+            #create jwt token
+            payload = {user_id: user.id}
+            token = JWT.encode(payload, 'my_secret', 'HS256')
+            
+            render json: {user: {id: user.id, username: user.username}, token: token}   
+        else
+            render json: {error: 'Invalid username/password'}, status: 401
+         end
     end
 
 end
